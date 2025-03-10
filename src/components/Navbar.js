a9e005303f0d6639b3b670ab357d70a696a3b8ea
dlaister <derek_laister@outlook.com>
2025-03-10 00:48:37 -0700

    updated navbar with active page functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,10 +5,10 @@ function Navbar() {
     return (
         <nav className="navbar">
             <ul>
-                <li><NavLink exact to="/" activeClassName="active-page">Home</NavLink></li>
-                <li><NavLink to="/sample" activeClassName="active-page">The Game</NavLink></li>
-                <li><NavLink to="/rules" activeClassName="active-page">The Rules</NavLink></li>
-                <li><NavLink to="/scores" activeClassName="active-page">The Scores</NavLink></li>
+                <li><NavLink to="/" className={({ isActive }) => (isActive ? "active-page" : "")}>Home</NavLink></li>
+                <li><NavLink to="/sample" className={({ isActive }) => (isActive ? "active-page" : "")}>The Game</NavLink></li>
+                <li><NavLink to="/rules" className={({ isActive }) => (isActive ? "active-page" : "")}>The Rules</NavLink></li>
+                <li><NavLink to="/scores" className={({ isActive }) => (isActive ? "active-page" : "")}>The Scores</NavLink></li>
             </ul>
         </nav>
     );
